ff2dc285744e6af12fc752498d4e74075e8a3b99
Владислав Шадрин <ultraresonance@gmail.com>
2022-07-22 05:55:50 +0600

    Improved logger visual style

--- core/src/main/java/ecs/utils/Logger.java
+++ core/src/main/java/ecs/utils/Logger.java
@@ -16,14 +16,50 @@ import static ecs.utils.TerminalUtils.ANSI_RESET;
 public class Logger {
 
     public enum Level {
-        TRACE(0),
-        DEBUG(1),
-        INFO(2),
-        WARN(3),
-        ERROR(4),
-        FATAL(5);
+        TRACE(0) {
+            @Override
+            public String formatLevel() {
+                return enclose(name());
+            }
+        },
+        DEBUG(1) {
+            @Override
+            public String formatLevel() {
+                return ANSI_BRIGHT_BLUE + enclose(name()) + ANSI_RESET;
+            }
+        },
+        INFO(2) {
+            @Override
+            public String formatLevel() {
+                return enclose(name());
+            }
+        },
+        WARN(3) {
+            @Override
+            public String formatLevel() {
+                return ANSI_YELLOW + enclose(name()) + ANSI_RESET;
+            }
+        },
+        ERROR(4) {
+            @Override
+            public String formatLevel() {
+                return ANSI_RED + enclose(name()) + ANSI_RESET;
+            }
+        },
+        FATAL(5) {
+            @Override
+            public String formatLevel() {
+                return ANSI_BRIGHT_RED + enclose(name()) + ANSI_RESET;
+            }
+        };
 
         private int priority;
+
+        public abstract String formatLevel();
+
+        private static String enclose(String name) {
+            return String.format("%7s", name);
+        }
 
         Level(int priority) {
             this.priority = priority;
@@ -44,6 +80,7 @@ public class Logger {
     public static class OutputFilePrinter implements Printer {
 
         private OutputStream stream;
+        private Printer attachedPrinter;
 
         public OutputFilePrinter(String filePath) {
             try {
@@ -57,33 +94,39 @@ public class Logger {
         @Override
         public void print(String message) {
             try {
-                stream.write(message.getBytes(StandardCharsets.ISO_8859_1));
+                stream.write(message.getBytes(StandardCharsets.UTF_8));
+                attachedPrinter.print(message);
             } catch (IOException e) {
                 error(e.getMessage());
                 throw new RuntimeException(e);
             }
         }
+
+        public OutputFilePrinter attach(Printer printer) {
+            this.attachedPrinter = printer;
+            return this;
+        }
     }
 
     public static final Level LEVEL = ApplicationConfig.LOGGER_SEVERITY;
-    public static final String dateTimeFormatPattern = "HH:mm:ss.SSS";
-    public static final Printer printer = new OutputFilePrinter(ApplicationConfig.LOG_FILE_PATH);
 
+    public static final String dateTimeFormatPattern = "HH:mm:ss.SSS";
+    public static final Printer printer = new ConsolePrinter();
     private static final DateFormat dateFormat = new SimpleDateFormat(dateTimeFormatPattern);
 
-    private static void printf(String level, String message) {
+    private static void printf(Level level, String message) {
         printer.print(String.format(
-                "\n" + ANSI_CYAN + "[%s]" + ANSI_RESET + " [%8s] [%s]: %s",
+                "\n" + ANSI_CYAN + "%s" + ANSI_RESET + "  [%8s] %s: %s",
                 dateFormat.format(Date.from(Instant.now())),
                 Thread.currentThread().getName(),
-                level,
+                level.formatLevel(),
                 message
         ));
     }
 
     public static void log(Level level, String message) {
         if (level.priority < LEVEL.priority) return;
-        printf(level.name(), message);
+        printf(level, message);
     }
 
     public static void trace(String message) {
