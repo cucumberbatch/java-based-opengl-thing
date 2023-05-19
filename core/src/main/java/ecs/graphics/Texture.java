4d0b8b2fe24c85462fb867c4980689190a1bfae6
cucumberbatch <ultraresonance@gmail.com>
2023-05-19 12:25:20 +0600

    Caching textures in map

--- core/src/main/java/ecs/graphics/Texture.java
+++ core/src/main/java/ecs/graphics/Texture.java
@@ -16,8 +16,17 @@ public class Texture {
     private int width, height;
     private int texture;
 
+    public Texture(int width, int height, int texture) {
+        this.width = width;
+        this.height = height;
+        this.texture = texture;
+    }
+
     public Texture(String path) {
-        texture = load(path);
+        Texture textureInstance = TextureManager.tryToLoadTexture(path);
+        this.width = textureInstance.width;
+        this.height = textureInstance.height;
+        this.texture = textureInstance.texture;
     }
 
     public int getWidth() {
@@ -26,45 +35,6 @@ public class Texture {
 
     public int getHeight() {
         return height;
-    }
-
-    private int load(String path) {
-        int[] pixels = null;
-        FileInputStream stream;
-        try {
-            stream = new FileInputStream(path);
-            BufferedImage image = ImageIO.read(stream);
-            width = image.getWidth();
-            height = image.getHeight();
-            pixels = new int[width * height];
-            image.getRGB(0, 0, width, height, pixels, 0, width);
-        } catch (FileNotFoundException e) {
-            Logger.error(String.format("Texture '<underline>%s</>' not found!", path), e);
-        } catch (SecurityException e) {
-            Logger.error(String.format("Texture file access denied on path '<underline>%s</>'!", path), e);
-        } catch (IllegalArgumentException e) {
-            Logger.error(String.format("Incorrect texture file path '<underline>%s</>'!", path), e);
-        } catch (IOException e) {
-            Logger.error(String.format("Error while loading texture '<underline>%s</>'!", path), e);
-        }
-
-        int[] data = new int[width * height];
-        for (int i = 0; i < width * height; i++) {
-            int a = (pixels[i] & 0xff000000) >> 24;
-            int r = (pixels[i] & 0xff0000) >> 16;
-            int g = (pixels[i] & 0xff00) >> 8;
-            int b = (pixels[i] & 0xff);
-
-            data[i] = a << 24 | b << 16 | g << 8 | r;
-        }
-
-        int result = glGenTextures();
-        glBindTexture(GL_TEXTURE_2D, result);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, BufferUtils.createIntBuffer(data));
-        glBindTexture(GL_TEXTURE_2D, 0);
-        return result;
     }
 
     public void bind() {
