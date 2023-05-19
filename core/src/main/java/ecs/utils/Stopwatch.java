a23dc3cbdd3c4cdc15209935feb2f9239e19d5b3
cucumberbatch <ultraresonance@gmail.com>
2023-05-19 22:50:18 +0600

    Stopwatch improvements for nested measures

--- core/src/main/java/ecs/utils/Stopwatch.java
+++ core/src/main/java/ecs/utils/Stopwatch.java
@@ -1,16 +1,17 @@
 package ecs.utils;
 
+import java.util.Stack;
+
 public class Stopwatch {
-    // todo: better to add a stack structure to do nested measurement
-    private static long timeInNanos;
+    private static final Stack<Long> NANO_TIME_STACK = new Stack<>();
 
     public static void start() {
-        timeInNanos = System.nanoTime();
+        NANO_TIME_STACK.add(System.nanoTime());
     }
 
     public static void stop(String messageTemplate) {
-        float diffMillis = (float) (java.lang.System.nanoTime() - timeInNanos) / 1_000_000L;
-        Logger.debug(String.format(messageTemplate, diffMillis));
+        float diffMillis = (float) (System.nanoTime() - NANO_TIME_STACK.pop()) / 1_000_000L;
+        Logger.debug(String.format("Stopwatch: <bold>%.3f[ms]</> " + messageTemplate, diffMillis));
     }
 
 }
